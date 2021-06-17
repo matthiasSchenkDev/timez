6bbaaada470228e43a405537a4e13011d0300f11
Matthias Schenk <>
2021-06-17 19:59:52 +0200

    add express public folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const port = process.env.port || 3000;
-app.use(express.static(path.join(__dirname, './VideoCallPage')));
+app.use(express.static('public'));
 
 app.listen(port, () => {
 	console.log("Server running at port: " + port);
